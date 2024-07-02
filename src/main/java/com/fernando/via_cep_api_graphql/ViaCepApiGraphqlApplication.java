95be0450ec3efe59763fc8dd8df1bef7b695e033
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-07-02 17:10:09 -0300

    Enable FeignClient in spring boot application

--- src/main/java/com/fernando/via_cep_api_graphql/ViaCepApiGraphqlApplication.java
+++ src/main/java/com/fernando/via_cep_api_graphql/ViaCepApiGraphqlApplication.java
@@ -2,8 +2,12 @@ package com.fernando.via_cep_api_graphql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
+@EnableFeignClients
 public class ViaCepApiGraphqlApplication {
 
 	public static void main(String[] args) {
